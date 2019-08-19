3a9406a63ecdb572e030bf897d6c07a763a7ba28
canwaykalburim <canwaykalburim@gmail.com>
2019-08-19 09:35:11 +0900

    Love 2D 기초

---
+++ Love2d-basic/main.lua
@@ -0,0 +1,13 @@
+function love.load(arg)
+  -- body
+  love.window.setTitle("print \"Hello, world\"")
+end
+
+function love.update(dt)
+  --body
+end
+
+function love.draw()
+  -- body
+  love.graphics.print("Hello, world")
+end
